dda6944ca6ddd4e8b923b0dfd789971a69b1a733
Dave Smith-Hayes <dsmithhayes@gmail.com>
2017-01-17 13:30:14 -0500

    Added a test for the service provider.

---
+++ INSTRUCTIONS.md
@@ -0,0 +1,8 @@
+# Reddin Angular Challenge
+
+The angular app is very basic to set up and only requires that the user has
+`npm` and `bower` installed.
+
+    $ npm install
+    $ bower install
+    $ npm start
---
+++ app/tests/csv-service_test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('csvService', function() {
+  var service, httpBackend;
+
+  beforeEach(module('reddin'));
+
+  beforeEach(function () {
+    inject(function ($httpBackend, csvService) {
+      httpBackend = $httpBackend;
+      service = csvService;
+    })
+  });
+
+  afterEach(function() {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should get the raw CSV data and perform the parsing.', function () {
+    var mockData = "first_name,last_name,email\n"
+      + "Dave,Smith-Hayes,me@davesmithhayes.com\n";
+
+    httpBackend.expectGET('data/user-list.csv').respond(mockData);
+    service.getData();
+
+    httpBackend.flush();
+
+    expect(typeof service.data[0]).toBe('object');
+    expect(service.data[0].id).toBe(0);
+    expect(service.data[0].first_name).toBe('Dave');
+    expect(service.data[0].last_name).toBe('Smith-Hayes');
+  });
+});
